ba23d612e59ba9d1f1b0cec00e5f895597695768
SucioLoco <irelandrj@yahoo.com>
2021-10-20 18:21:11 -0400

    Animated happy valley shelter

---
+++ main.js
@@ -0,0 +1,12 @@
+gsap.to(".header",
+ {duration: 3,
+ x: 400,
+ backgroundColor: "#004175",
+ borderBottomWidth: "7px",
+ borderTopWidth: "0px",
+ borderLeftWidth: "0px",
+borderRightWidth:"0px",
+ border: "solid #89DEEF"
+})
+
+
